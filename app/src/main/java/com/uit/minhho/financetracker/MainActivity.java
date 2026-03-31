0db5b395047a39e211330a193b29fa72aff09a66
min-hit-1701 <minhcopxki@gmail.com>
2026-03-31 20:57:28 +0700

    Them bottom navigation

--- app/src/main/java/com/uit/minhho/financetracker/MainActivity.java
+++ app/src/main/java/com/uit/minhho/financetracker/MainActivity.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.uit.minhho.financetracker.ui.CardsFragment;
+import com.uit.minhho.financetracker.ui.FinanceFragment;
+import com.uit.minhho.financetracker.ui.HomeFragment;
+import com.uit.minhho.financetracker.ui.ProfileFragment;
+import com.uit.minhho.financetracker.ui.StatsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +23,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                fragment = new HomeFragment();
+            } else if (itemId == R.id.nav_finance) {
+                fragment = new FinanceFragment();
+            } else if (itemId == R.id.nav_stats) {
+                fragment = new StatsFragment();
+            } else if (itemId == R.id.nav_cards) {
+                fragment = new CardsFragment();
+            } else if (itemId == R.id.nav_profile) {
+                fragment = new ProfileFragment();
+            } else {
+                return false;
+            }
+
+            openFragment(fragment);
+            return true;
+        });
+
+        if (savedInstanceState == null) {
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
